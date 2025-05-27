6469d23c421541f725c052c0e938df024bab4844
Esther Wangari <essywangari06@gmail.com>
2025-05-27 23:52:30 +0300

    Fixed errors

--- coffee.py
+++ coffee.py
@@ -1,3 +1,5 @@
+from order import Order
+
 class Coffee:
     def __init__(self, name):
         if not isinstance(name, str) or len(name) < 3:
@@ -7,4 +9,20 @@ class Coffee:
     @property
     def name (self):
         return self._name 
+    
+    def orders(self):
+        return [order for order in Order.all if order.coffee == self]
+
+    def customers(self):
+        return list({order.customer for order in self.orders()})
+
+    def num_orders(self):
+        return len(self.orders())
+
+    def average_price(self):
+        orders = self.orders()
+        if not orders:
+            return 0
+        return sum(order.price for order in orders) / len(orders)
+
     
--- customer.py
+++ customer.py
@@ -1,6 +1,7 @@
+from order import Order
+
 class Customer:
     def __init__(self, name):
-        self.name = None
         self.name = name
 
     @property
@@ -12,3 +13,29 @@ class Customer:
             self._name = value
         else:
             raise ValueError("Name must be a string between 1 and 15 characters.") 
+        
+    def orders(self):
+        return [order for order in Order.all if order.customer == self]
+
+    def coffees(self):
+        return list({order.coffee for order in self.orders()})
+
+    def create_order(self, coffee, price):
+        return Order(self, coffee, price)
+
+    @classmethod
+    def most_aficionado(cls, coffee):
+        if not Order.all:
+            return None
+        
+        
+        totals = {}
+        for order in Order.all:
+            if order.coffee == coffee:
+                totals[order.customer] = totals.get(order.customer, 0) + order.price
+        
+        if not totals:
+            return None
+        
+        
+        return max(totals, key=totals.get)
